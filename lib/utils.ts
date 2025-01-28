4ce52cab06fa7b5e7333a320e40093cb30870358
renan <renanlemos21@gmail.com>
2025-01-28 18:43:27 -0300

    feat: add shadcn and theme

---
+++ lib/utils.ts
@@ -0,0 +1,10 @@
+import { clsx, type ClassValue } from 'clsx/lite'
+import { twMerge } from 'tailwind-merge'
+
+/**
+ * Uses `clsx` to merge classes conditionally and `tailwind-merge` avoid class duplication.
+ * @param inputs
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
