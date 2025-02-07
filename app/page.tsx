ec296e92758ff7557feaeb1a90097e4f9568c6a6
renan <renanlemos21@gmail.com>
2025-02-07 17:57:31 -0300

    feat: add professors list

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import { Hero } from '@/components/Hero/Hero'
+import { ProfessorsList } from '@/components/ProfessorsList/ProfessorsList'
 
 export default function Home() {
   return (
     <main>
       <Hero/>
+      <section className={'w-full grid grid-cols-2 gap-4 px-4 py-8'}>
+        <div className={'container'}>
+          <h2 className={'text-2xl mb-10 md:text-4xl max-w-2xl tracking-tighter text-center font-regular'}>Professores</h2>
+          <ProfessorsList/>
+        </div>
+        <div className={'container'}>
+          <h2 className={'text-2xl mb-10 md:text-4xl max-w-2xl tracking-tighter text-center font-regular'}>Disciplinas</h2>
+        </div>
+      </section>
     </main>
   );
 }
--- services/professors/professors.ts
+++ services/professors/professors.ts
@@ -1,6 +1,30 @@
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+
 export interface Professor	{
   "id": string,
   "name": string,
   "email": string,
   "profileImg": string | null
+}
+
+const instance = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
+  timeout: 3000,
+})
+
+interface Options {
+  offset?: number
+  limit?: number
+}
+
+export const useGetAllProfessors = (options?: Options) => {
+  return useQuery({
+    queryKey: ['professors', options],
+    queryFn: async () => {
+      const {offset = 0, limit = 10 } = options ?? {}
+      const { data } = await instance.get<Professor[]>(`/professors?limit=${limit}&offset=${offset}`)
+      return data
+    },
+  })
 }
