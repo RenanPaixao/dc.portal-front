41700398eb7ff15e3ef8a552130de7e0da534a2e
renan <renanlemos21@gmail.com>
2025-03-11 10:11:50 -0300

    feat: recover tailwind styles to work with shadcn components properly

--- app/routes/resources.search.tsx
+++ app/routes/resources.search.tsx
@@ -1,5 +1,5 @@
-import { LoaderFunctionArgs } from '@remix-run/node'
 import { search } from '@/services/search/search'
+import { LoaderFunctionArgs } from '@remix-run/node'
 
 export const loader = async ({request}: LoaderFunctionArgs) => {
   const url = new URL(request.url)
