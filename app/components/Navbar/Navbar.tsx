04c5631c0abd40811f919f30e63ff0cf2a8cd2f0
renan <renanlemos21@gmail.com>
2025-04-21 17:48:50 -0300

    feat: add logout button

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,11 +1,22 @@
 import { Button } from '@/app/components/common/Button/Button'
-import { CircleUserRound } from 'lucide-react'
+import { CircleUserRound, LogOut } from 'lucide-react'
 import { Form } from '@remix-run/react'
+import { useUser } from '~/hooks/useUser'
 
 export const Navbar = () => {
+  const {user} = useUser()
+  
   return <nav className={'flex justify-end p-8'}>
-    <Form method={'post'} action={'/auth/google'} navigate={false}>
-      <Button> <CircleUserRound /> Login</Button>
-    </Form>
+    {
+      user ? (
+        <Form method={'post'} action={'/auth/logout'} navigate={false}>
+          <Button variant={'destructive'}> <LogOut /> Logout</Button>
+        </Form>
+      ) : (
+        <Form method={'post'} action={'/auth/google'} navigate={false}>
+          <Button> <CircleUserRound /> Login</Button>
+        </Form>
+      )
+    }
   </nav>
 }
