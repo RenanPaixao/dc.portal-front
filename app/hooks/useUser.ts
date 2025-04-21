1e2f19bd852b55f8374cc5e3cfe0956b125a3164
renan <renanlemos21@gmail.com>
2025-04-21 18:00:12 -0300

    feat: add user typings

--- app/hooks/useUser.ts
+++ app/hooks/useUser.ts
@@ -1,9 +1,23 @@
 import { useRouteLoaderData } from '@remix-run/react'
 
+interface User {
+  "profile": {
+    "sub": string,
+    "name": string,
+    "given_name": string,
+    "family_name": string,
+    "picture": string,
+    "email": string,
+    "email_verified": boolean
+  },
+  "tokens": {
+    "access_token": string,
+    "expires_in": number
+  }
+}
+
 export const useUser = () => {
-  // TODO: add types
-  const { user = null } = useRouteLoaderData<{user: any | null}>('root') ?? {}
-  
+  const { user = null } = useRouteLoaderData<{user: User | null}>('root') ?? {}
   return {
     user
   }
